7710a49c29e22155125db4a43c1d56a28e70a886
Janne Jäppinen <janne.jappinen@protonmail.com>
2018-03-04 13:58:49 +0200

    Clean up store creation. Fix error when redux devtool extension is not installed.

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,8 +6,7 @@ export default function configureStore(initialState) {
   const sagaMiddleware = createSagaMiddleware();
   const enhancer = compose(
     applyMiddleware(sagaMiddleware),
-    // window.devToolsExtension ? window.devToolsExtension() : f => f,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : compose,
   );
 
   const store = createStore(
@@ -21,41 +20,12 @@ export default function configureStore(initialState) {
   });
 
   if (module.hot) {
-    // Enable Webpack hot module replacement for reducers
     module.hot.accept(rootReducer, () => {
       const nextReducer = rootReducer;
 
       store.replaceReducer(nextReducer);
     });
   }
-
-  // if (module.hot) {
-  //   module.hot.accept();
-  // }
-
-  // if (module.hot) {
-  //   module.hot.accept(rootReducer, () => {
-  //     const nextReducer = rootReducer;
-  //     store.replaceReducer(nextReducer);
-  //   });
-  // }
-
-  // if (module.hot) {
-  //   // Enable webpack hot module replacement for reducers
-  //   module.hot.accept(
-  //     './reducers',
-  //     () => store.replaceReducer(rootReducer)
-  //   );
-  // }
-
-  // if (module.hot) {
-  //   // Enable Webpack hot module replacement for reducers
-  //   module.hot.accept('./reducers', () => {
-  //     const nextReducer = require('./reducers/index').default;
-  //
-  //     store.replaceReducer(nextReducer);
-  //   });
-  // }
 
   sagaMiddleware.run(rootSaga);
 
