910c3a9166b77ef07655f519d4a09651998f7402
Janne Jäppinen <janne.jappinen@protonmail.com>
2018-03-04 13:59:35 +0200

    Webpack conf host 0.0.0.0 for docker.

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,7 @@ module.exports = {
   devServer: {
     contentBase: path.resolve(__dirname, 'public'),
     historyApiFallback: true,
+    host: '0.0.0.0',
     port: 3000,
     filename: 'bundle.js',
   },
